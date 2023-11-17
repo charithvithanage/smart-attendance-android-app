15c84b761fb7e0da197871df21c740a8754fcd82
charithvin@gmail.com <charithvin@gmail.com>
2023-11-17 11:11:32 +0530

    Optimized login page code Updated dependencies

--- user-android-app/app/src/main/java/lnbti/charithgtp01/smartattendanceuserapp/ui/login/LoginActivity.kt
+++ user-android-app/app/src/main/java/lnbti/charithgtp01/smartattendanceuserapp/ui/login/LoginActivity.kt
@@ -58,189 +58,204 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val loggedInUser = getObjectFromSharedPref(this@LoginActivity, LOGGED_IN_USER)
-        //Already user is logged in navigate to home page
-        if (loggedInUser != null) {
-            navigateToAnotherActivity(
-                this@LoginActivity,
-                MainActivity::class.java
-            )
-        } else {
-            initiateDataBinding()
-            initiateView()
-            viewModelObservers()
+        getObjectFromSharedPref(this@LoginActivity, LOGGED_IN_USER).let { loggedInUser ->
+            //Already user is logged in navigate to home page
+            when {
+                loggedInUser != null -> navigateToAnotherActivity(
+                    this@LoginActivity,
+                    MainActivity::class.java
+                )
+
+                else -> {
+                    initiateDataBinding()
+                    viewModelObservers()
+                }
+            }
         }
+
 
     }
 
     private fun initiateDataBinding() {
-        binding =
-            DataBindingUtil.setContentView(this, R.layout.activity_login)
-
-        //Data binding
-        loginViewModel = ViewModelProvider(this)[LoginViewModel::class.java]
-        binding.vm = loginViewModel
-        binding.lifecycleOwner = this
-    }
-
-    private fun initiateView() {
-        changeUiSize(this, binding.appLogo, 1, 1, 30)
-        username = binding.username
-        usernameInputText = binding.usernameInputText
-        password = binding.etPassword
-        passwordInputText = binding.passwordInputText
-        login = binding.login
-
-        login.setOnClickListener {
-            loginViewModel.loginDataChanged(
-                username.text.toString(),
-                password.text.toString()
+        ViewModelProvider(this)[LoginViewModel::class.java].apply {
+            //Data binding
+            loginViewModel = this
+            DataBindingUtil.setContentView<ActivityLoginBinding?>(
+                this@LoginActivity,
+                R.layout.activity_login
             )
-        }
+                .apply {
+                    binding = this
+                    vm = loginViewModel
+                    lifecycleOwner = this@LoginActivity
 
-        binding.bioMetricAuthentication.setOnClickListener {
-            val sharedPref = getSharedPreferences(
-                getString(R.string.preference_file_key),
-                Context.MODE_PRIVATE
-            )
+                    changeUiSize(this@LoginActivity, binding.appLogo, 1, 1, 30)
+                    this@LoginActivity.username = binding.username
+                    this@LoginActivity.usernameInputText = binding.usernameInputText
+                    this@LoginActivity.password = binding.etPassword
+                    this@LoginActivity.passwordInputText = binding.passwordInputText
+                    this@LoginActivity.login = binding.login
 
-            val bioMetricAuthenticationEnableStatus =
-                sharedPref.getBoolean(ResourceConstants.BIO_METRIC_ENABLE_STATUS, false)
-            val lastSignInObject =
-                getObjectFromSharedPref(
-                    this@LoginActivity,
-                    ResourceConstants.LAST_LOGGED_IN_CREDENTIAL
-                )
-            if (bioMetricAuthenticationEnableStatus && lastSignInObject != null) {
-                val biometricHelper =
-                    BiometricAuthenticationHelper(this) // 'this' should be a valid context
-                biometricHelper.authenticateBiometric(
-                    "Biometric Authentication",
-                    "Verify your identity",
-                    "Place your finger on the sensor",
-                    {
-                        // Biometric authentication successful
-                        // You can perform your actions here
+                    login.setOnClickListener {
+                        loginDataChanged(
+                            username.text.toString(),
+                            password.text.toString()
+                        )
+                    }
 
-
-                        var decryptedCredential: String? = null
-                        try {
-                            decryptedCredential = decrypt(SECURE_KEY, lastSignInObject)
-                        } catch (e: Exception) {
-                            e.printStackTrace()
-                        }
-                        val credential = gson.fromJson(decryptedCredential, Credential::class.java)
-
-                        dialog = showProgressDialog(this, getString(R.string.wait))
-                        val deviceID = getAndroidId(this@LoginActivity)
-                        loginViewModel.login(
-                            deviceID,
-                            credential.username,
-                            credential.password
+                    bioMetricAuthentication.setOnClickListener {
+                        val sharedPref = getSharedPreferences(
+                            getString(R.string.preference_file_key),
+                            Context.MODE_PRIVATE
                         )
 
-                    },
-                    { error ->
-                        // Handle authentication error, e.g., show an error message
-                        showErrorDialog(this@LoginActivity, "Login Error")
+                        val bioMetricAuthenticationEnableStatus =
+                            sharedPref.getBoolean(ResourceConstants.BIO_METRIC_ENABLE_STATUS, false)
+                        val lastSignInObject =
+                            getObjectFromSharedPref(
+                                this@LoginActivity,
+                                ResourceConstants.LAST_LOGGED_IN_CREDENTIAL
+                            )
+                        if (bioMetricAuthenticationEnableStatus && lastSignInObject != null) {
+                            val biometricHelper =
+                                BiometricAuthenticationHelper(this@LoginActivity) // 'this' should be a valid context
+                            biometricHelper.authenticateBiometric(
+                                "Biometric Authentication",
+                                "Verify your identity",
+                                "Place your finger on the sensor",
+                                {
+                                    // Biometric authentication successful
+                                    // You can perform your actions here
+
+
+                                    var decryptedCredential: String? = null
+                                    try {
+                                        decryptedCredential = decrypt(SECURE_KEY, lastSignInObject)
+                                    } catch (e: Exception) {
+                                        e.printStackTrace()
+                                    }
+                                    val credential =
+                                        gson.fromJson(decryptedCredential, Credential::class.java)
+
+                                    dialog = showProgressDialog(this@LoginActivity, getString(R.string.wait))
+                                    val deviceID = getAndroidId(this@LoginActivity)
+                                    login(
+                                        deviceID,
+                                        credential.username,
+                                        credential.password
+                                    )
+
+                                },
+                                {
+                                    // Handle authentication error, e.g., show an error message
+                                    showErrorDialog(this@LoginActivity, "Login Error")
+                                }
+                            )
+                        } else {
+                            showErrorDialog(
+                                this@LoginActivity,
+                                getString(R.string.bio_metric_disabled)
+                            )
+                        }
                     }
-                )
-            } else {
-                showErrorDialog(this@LoginActivity, getString(R.string.bio_metric_disabled))
-            }
-        }
 
-        //After 4 click on the logo navigate to Device ID qr activity
-        binding.appLogo.setOnClickListener {
-            i = atomicInteger.getAndIncrement()
-            if (i >= 4) {
-                atomicInteger = AtomicInteger(0)
-                i = 0
-                val androidID = getAndroidId(this)
-                goToQRActivity(androidID)
-            }
-        }
+                    //After 4 click on the logo navigate to Device ID qr activity
+                    appLogo.setOnClickListener {
+                        i = atomicInteger.getAndIncrement()
+                        if (i >= 4) {
+                            atomicInteger = AtomicInteger(0)
+                            i = 0
+                            val androidID = getAndroidId(this@LoginActivity)
+                            goToQRActivity(androidID)
+                        }
+                    }
 
-        binding.btnSignUp.setOnClickListener {
-            navigateToAnotherActivity(
-                this@LoginActivity,
-                SearchCompanyActivity::class.java
-            )
+                    btnSignUp.setOnClickListener {
+                        navigateToAnotherActivity(
+                            this@LoginActivity,
+                            SearchCompanyActivity::class.java
+                        )
+                    }
+                }
         }
     }
 
     private fun viewModelObservers() {
-        loginViewModel.loginFormState.observe(this@LoginActivity, Observer {
-            val loginState = it ?: return@Observer
 
-            if (loginState.nicError != null) {
-                usernameInputText.error = getString(loginState.nicError)
-            } else
-                validState(usernameInputText, R.drawable.ic_check)
+        loginViewModel.apply {
+            loginFormState.observe(this@LoginActivity, Observer {
+                val loginState = it ?: return@Observer
+
+                if (loginState.nicError != null) {
+                    usernameInputText.error = getString(loginState.nicError)
+                } else
+                    validState(usernameInputText, R.drawable.ic_check)
 
 
-            if (loginState.passwordError != null) {
-                passwordInputText.error = getString(loginState.passwordError)
-            }
-            /* Need to enter above data to successful login
-               "email": "eve.holt@reqres.in",
-               "password": "cityslicka"
-            */
-            if (loginState.isDataValid) {
-                dialog = showProgressDialog(this, getString(R.string.wait))
-                val deviceID = getAndroidId(this@LoginActivity)
-                loginViewModel.login(
-                    deviceID,
-                    username.text.toString(),
-                    password.text.toString()
-                )
-            }
-        })
-
-        loginViewModel.loginResult.observe(this@LoginActivity, Observer {
-            val loginResult = it ?: return@Observer
-
-            dialog?.dismiss()
-
-            if (loginResult.success) {
-                val loggedInUser = gson.fromJson(loginResult.data.toString(), User::class.java)
-                val hashMap = HashMap<String, String>()
-
-                // Add values to the HashMap
-                hashMap[LOGGED_IN_USER] = loginResult.data.toString()
-                hashMap[USER_ROLE] = loggedInUser.userRole
-
-                try {
-                    val credential = Credential(
+                if (loginState.passwordError != null) {
+                    passwordInputText.error = getString(loginState.passwordError)
+                }
+                /* Need to enter above data to successful login
+                   "email": "eve.holt@reqres.in",
+                   "password": "cityslicka"
+                */
+                if (loginState.isDataValid) {
+                    dialog = showProgressDialog(this@LoginActivity, getString(R.string.wait))
+                    val deviceID = getAndroidId(this@LoginActivity)
+                    loginViewModel.login(
+                        deviceID,
                         username.text.toString(),
                         password.text.toString()
                     )
-
-                    val encryptedCredential = encrypt(SECURE_KEY, gson.toJson(credential))
-                    hashMap[ResourceConstants.LAST_LOGGED_IN_CREDENTIAL] =
-                        encryptedCredential.toString()
-
-                } catch (e: Exception) {
-
                 }
+            })
 
-                if (loggedInUser.userType == ResourceConstants.ANDROID_USER) {
-                    saveMultipleObjectsInSharedPref(this@LoginActivity, hashMap,
-                        SuccessListener {
-                            navigateToAnotherActivity(
-                                this@LoginActivity,
-                                MainActivity::class.java
-                            )
-                        })
+            loginResult.observe(this@LoginActivity, Observer {
+                val loginResult = it ?: return@Observer
+
+                dialog?.dismiss()
+
+                if (loginResult.success) {
+                    val loggedInUser = gson.fromJson(loginResult.data.toString(), User::class.java)
+                    val hashMap = HashMap<String, String>()
+
+                    // Add values to the HashMap
+                    hashMap[LOGGED_IN_USER] = loginResult.data.toString()
+                    hashMap[USER_ROLE] = loggedInUser.userRole
+
+                    try {
+                        val credential = Credential(
+                            username.text.toString(),
+                            password.text.toString()
+                        )
+
+                        val encryptedCredential = encrypt(SECURE_KEY, gson.toJson(credential))
+                        hashMap[ResourceConstants.LAST_LOGGED_IN_CREDENTIAL] =
+                            encryptedCredential.toString()
+
+                    } catch (e: Exception) {
+
+                    }
+
+                    if (loggedInUser.userType == ResourceConstants.ANDROID_USER) {
+                        saveMultipleObjectsInSharedPref(this@LoginActivity, hashMap,
+                            SuccessListener {
+                                navigateToAnotherActivity(
+                                    this@LoginActivity,
+                                    MainActivity::class.java
+                                )
+                            })
+                    } else {
+                        showErrorDialog(this@LoginActivity, getString(R.string.no_permission_to_use))
+                    }
+
                 } else {
-                    showErrorDialog(this, getString(R.string.no_permission_to_use))
+                    showErrorDialog(this@LoginActivity, loginResult.message)
                 }
 
-            } else {
-                showErrorDialog(this, loginResult.message)
-            }
+            })
+        }
 
-        })
     }
 
     /**
