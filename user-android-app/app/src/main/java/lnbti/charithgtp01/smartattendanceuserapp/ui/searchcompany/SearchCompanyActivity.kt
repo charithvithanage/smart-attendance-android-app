8296c80a9e11d22e9fd98dc08082a5b543ce31ac
charithvin@gmail.com <charithvin@gmail.com>
2023-11-17 12:08:22 +0530

    Optimized searchcompany folder files Add KDOC documentations

--- user-android-app/app/src/main/java/lnbti/charithgtp01/smartattendanceuserapp/ui/searchcompany/SearchCompanyActivity.kt
+++ user-android-app/app/src/main/java/lnbti/charithgtp01/smartattendanceuserapp/ui/searchcompany/SearchCompanyActivity.kt
@@ -11,18 +11,23 @@ import dagger.hilt.android.AndroidEntryPoint
 import lnbti.charithgtp01.smartattendanceuserapp.R
 import lnbti.charithgtp01.smartattendanceuserapp.constants.Constants
 import lnbti.charithgtp01.smartattendanceuserapp.databinding.ActivitySearchCompanyBinding
-import lnbti.charithgtp01.smartattendanceuserapp.interfaces.CustomAlertDialogListener
 import lnbti.charithgtp01.smartattendanceuserapp.ui.register.RegisterActivity
-import lnbti.charithgtp01.smartattendanceuserapp.utils.DialogUtils
-import lnbti.charithgtp01.smartattendanceuserapp.utils.DialogUtils.Companion.showAlertDialog
 import lnbti.charithgtp01.smartattendanceuserapp.utils.DialogUtils.Companion.showErrorDialog
 import lnbti.charithgtp01.smartattendanceuserapp.utils.DialogUtils.Companion.showProgressDialog
-import lnbti.charithgtp01.smartattendanceuserapp.utils.UIUtils
+import lnbti.charithgtp01.smartattendanceuserapp.utils.NetworkUtils
 import lnbti.charithgtp01.smartattendanceuserapp.utils.UIUtils.Companion.initiateActionBarWithoutHomeButton
 import lnbti.charithgtp01.smartattendanceuserapp.utils.UIUtils.Companion.validState
-import lnbti.charithgtp01.smartattendanceuserapp.utils.Utils
 import lnbti.charithgtp01.smartattendanceuserapp.utils.Utils.Companion.navigateToAnotherActivityWithExtras
 
+/**
+ * Activity for searching companies.
+ *
+ * This activity is responsible for allowing users to search for companies.
+ *
+ * @property searchCompanyViewModel ViewModel for managing data related to company search.
+ * @property binding Data binding for the activity.
+ * @property dialog Dialog fragment for showing progress or error messages.
+ */
 @AndroidEntryPoint
 class SearchCompanyActivity : AppCompatActivity() {
 
@@ -32,81 +37,98 @@ class SearchCompanyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         initiateDataBinding()
-        initiateView()
         viewModelObservers()
     }
 
+    /**
+     * Initializes data binding for the activity.
+     */
     private fun initiateDataBinding() {
-        binding =
-            DataBindingUtil.setContentView(this, R.layout.activity_search_company)
 
         //Data binding
-        searchCompanyViewModel = ViewModelProvider(this)[SearchCompanyViewModel::class.java]
-        binding.vm = searchCompanyViewModel
-        binding.lifecycleOwner = this
-    }
+        ViewModelProvider(this)[SearchCompanyViewModel::class.java].apply {
+            searchCompanyViewModel =this
+            binding =
+                DataBindingUtil.setContentView<ActivitySearchCompanyBinding?>(this@SearchCompanyActivity, R.layout.activity_search_company).apply {
+                    vm = searchCompanyViewModel
+                    lifecycleOwner = this@SearchCompanyActivity
 
-    private fun initiateView() {
-        initiateActionBarWithoutHomeButton(
-            binding?.actionBar?.mainLayout!!,
-            getString(R.string.search_company)
-        ) { onBackPressed() }
+                    initiateActionBarWithoutHomeButton(
+                        actionBar.mainLayout,
+                        getString(R.string.search_company)
+                    ) { onBackPressed() }
 
-        binding.btnSubmit.setOnClickListener {
-            searchCompanyViewModel.searchCompanyDataChanged()
+                    btnSubmit.setOnClickListener {
+                        searchCompanyDataChanged()
+                    }
+                }
         }
+
     }
 
+    /**
+     * Observes changes in the ViewModel and takes appropriate actions.
+     */
     private fun viewModelObservers() {
-        /* Show error message in the custom error dialog */
-        searchCompanyViewModel.errorMessage.observe(this@SearchCompanyActivity) {
-            showErrorDialog(
-                this@SearchCompanyActivity,
-                it
-            )
-        }
-
-        searchCompanyViewModel.isDialogVisible.observe(this@SearchCompanyActivity) {
-            if (it) {
-                /* Show dialog when calling the API */
-                dialog = showProgressDialog(
+        searchCompanyViewModel.apply {
+            // Handle error messages
+            errorMessage.observe(this@SearchCompanyActivity) {
+                showErrorDialog(
                     this@SearchCompanyActivity,
-                    getString(R.string.wait)
+                    it
                 )
-            } else {
-                /* Dismiss dialog after updating the data list to recycle view */
-                dialog?.dismiss()
+            }
+
+            // Show or dismiss progress dialog
+            isDialogVisible.observe(this@SearchCompanyActivity) {
+                when {
+                    it -> dialog = showProgressDialog(
+                        this@SearchCompanyActivity,
+                        getString(R.string.wait)
+                    )
+                    else -> dialog?.dismiss()
+                }
+                // Show dialog when calling the API
+                // Dismiss dialog after updating the data list to recycle view
+            }
+
+            // Observe form state for searchCompanyForm
+            searchCompanyForm.observe(this@SearchCompanyActivity, Observer {
+                val formState = it ?: return@Observer
+
+                formState.apply {
+                    if (companyIDError != null) binding.etCompanyID.error =
+                        getString(companyIDError) else
+                        validState(binding.companyIDInputText, R.drawable.ic_check)
+
+                    isDataValid.let {
+                        // If Network available, call the backend API
+                        when {
+                            NetworkUtils.isNetworkAvailable() -> searchCompany()
+                            else -> setErrorMessage(getString(R.string.no_internet))
+                        }
+                    }
+                }
+
+            })
+
+
+            // Waiting for API response
+            searchCompanyResult.observe(this@SearchCompanyActivity) {
+                Gson().apply {
+                    HashMap<String, String>().apply {
+                        this[Constants.OBJECT_STRING] = toJson(it?.data)
+                        navigateToAnotherActivityWithExtras(
+                            this@SearchCompanyActivity,
+                            RegisterActivity::class.java,
+                            this
+                        )
+                    }
+                }
             }
         }
-        searchCompanyViewModel.searchCompanyForm.observe(this@SearchCompanyActivity, Observer {
-            val formState = it ?: return@Observer
-            if (formState.companyIDError != null) {
-                binding.etCompanyID?.error =
-                    getString(formState.companyIDError)
-            } else
-                validState(binding.companyIDInputText, R.drawable.ic_check)
 
-            if (formState.isDataValid) {
-                 searchCompanyViewModel.searchCompany()
-            }
-        })
-
-
-        //Waiting for Api response
-        searchCompanyViewModel.searchCompanyResult.observe(this@SearchCompanyActivity) {
-
-            val gson = Gson()
-            val prefMap = HashMap<String, String>()
-            prefMap[Constants.OBJECT_STRING] = gson.toJson(it?.data)
-            navigateToAnotherActivityWithExtras(
-                this,
-                RegisterActivity::class.java,
-                prefMap
-            )
-
-        }
     }
 }
 
--- user-android-app/app/src/main/java/lnbti/charithgtp01/smartattendanceuserapp/ui/searchcompany/SearchCompanyFormState.kt
+++ user-android-app/app/src/main/java/lnbti/charithgtp01/smartattendanceuserapp/ui/searchcompany/SearchCompanyFormState.kt
@@ -1,7 +1,11 @@
 package lnbti.charithgtp01.smartattendanceuserapp.ui.searchcompany
 
 /**
- * Data validation state of the login form.
+ * Represents the state of a search form for a company.
+ *
+ * @property companyIDError The error code associated with the company ID input. Set to `null` if there is no error.
+ * @property isDataValid Indicates whether the form data is valid or not. Default is `false`.
+ * 
  */
 data class SearchCompanyFormState(
     val companyIDError: Int? = null,
